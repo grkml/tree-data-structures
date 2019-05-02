f10025a5bfb27c902539f1041fc374d2692d8e0f
Gurkamal Singh <gurkamalpsc@gmail.com>
2019-05-02 00:00:05 +0000

    renamed some variables

--- general-tree-data-structure/src/main/java/general_tree_data_structure/Tree.java
+++ general-tree-data-structure/src/main/java/general_tree_data_structure/Tree.java
@@ -10,15 +10,34 @@ public class Tree<E>
    public int size() { return mSize; }
    public void clear() { mSize = 0; mRoot = null; }
    
+   // Client passes in some data to find. call overloaded recursive version find()
+   public TreeNode<E> find(E data) { 
+      return find(mRoot, data, 0); 
+   }
+   
+   // Overloaded find() for recursion
+   public TreeNode<E> find(TreeNode<E> root, E data, int level) {
+      TreeNode<E> returnValue;
+      
+      // Basecase 1 - main tree or recursive subtree is empty
+      // maybe we have looked all the way down to some leaf and still have not found our x
+      if (mSize == 0 || root == null)
+         return null;
+         
+      // Basecase 2 - Root of main tree or recursive subtree is node we're looking for
+      if (root.data.equals(data))
+         return root;
+         
+      // If past root of whatever subtree were on, recurse to sibling on the right, recursion will go go down its subtree
+      if ( level > 0 && (returnValue = find(root.nextSibling, data, level)) != null )
+         return returnValue;
+         
+      return find(root.firstChild, data, ++level);
+   }
+   
    // public TreeNode<E> addChild( TreeNode<E> treeNode,  E x )
    // {
    //    // ...
    // }
    
-   // public TreeNode<E> find(E x) { return find(mRoot, x, 0); }
-   
-   // public TreeNode<E> find(TreeNode<E> root, E x, int level)
-   // {
-   //    // ...
-   // }
 }
