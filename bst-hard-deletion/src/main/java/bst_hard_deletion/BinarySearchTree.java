796e2a72fcbe2b64d3bf8089d0d307fdc737c21f
Gurkamal Singh <gurkamalpsc@gmail.com>
2019-05-03 23:55:14 +0000

    redoing the implementation

---
+++ bst-hard-deletion/src/main/java/bst_hard_deletion/BinarySearchTree.java
@@ -0,0 +1,9 @@
+package bst_hard_deletion;
+
+import java.util.*;
+
+public class BinarySearchTree<E extends Comparable< ? super E > >
+   implements Cloneable
+{
+   
+}
---
+++ bst-hard-deletion/src/main/java/bst_hard_deletion/BinaryTreeNode.java
@@ -0,0 +1,21 @@
+package bst_hard_deletion;
+
+public class BinaryTreeNode<E extends Comparable< ? super E > >
+{
+   // use public access so the tree or other classes can access members 
+   public BinaryTreeNode<E> lftChild, rtChild;
+   public E data;
+   public BinaryTreeNode<E> myRoot;  // needed to test for certain error
+
+   public BinaryTreeNode( E d, BinaryTreeNode<E> lft, BinaryTreeNode<E> rt )
+   {
+      lftChild = lft; 
+      rtChild = rt;
+      data = d;
+   }
+   
+   public BinaryTreeNode()
+   {
+      this(null, null, null);
+   }
+}
