bc240523e8123d540c728e810e92c86e6f4c55c7
Gurkamal Singh <gurkamalpsc@gmail.com>
2019-05-03 23:22:17 +0000

    Started working on the TreeNode class for the bst-hard-deletion

--- bst-hard-deletion/src/main/java/bst_hard_deletion/TreeNode.java
+++ bst-hard-deletion/src/main/java/bst_hard_deletion/TreeNode.java
@@ -1,29 +1,21 @@
 package bst_hard_deletion;
 
-public class TreeNode<E> {
-    // use protected access so the tree, in the same package,
-    // or derived classes can access members  
-    protected TreeNode<E> firstChild, nextSibling, previousNode;
-    protected E data;
-    protected TreeNode<E> myRoot;  // needed to test for certain error
-    
-    public TreeNode(E data, TreeNode<E> nextSibling, TreeNode<E> firstChild, TreeNode<E> previousNode) {
-        this.firstChild = firstChild;
-        this.nextSibling = nextSibling;
-        this.previousNode = previousNode;
-        this.data = data;
-        myRoot = null;
-    }
-   
-    public TreeNode() {
-        this(null, null, null, null);
-    }
-   
-    public E getData() { return data; }
+package cs_1c;
 
-    // for use only by Tree (default access)
-    protected TreeNode( E data, TreeNode<E> nextSibling, TreeNode<E> firstChild, TreeNode<E> previousNode, TreeNode<E> root ) {
-        this(data, nextSibling, firstChild, previousNode);
-        myRoot = root;
-    }
+public class TreeNode<E extends Comparable< ? super E > >
+{
+   // use protected access so the tree, in the same package, 
+   // or derived classes can access members 
+   protected TreeNode<E> lftChild, rtChild;
+   protected E data;
+   protected TreeNode<E> myRoot; // needed to test for certain error
+
+   public TreeNode( E d, TreeNode<E> lft, TreeNode<E> rt )
+   {
+      lftChild = lft; 
+      rtChild = rt;
+      data = d;
+   }
+   
+   // a few less important methods omitted for now
 }
