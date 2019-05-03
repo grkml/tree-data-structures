9a66baedbc36ff6d2ba821f1d348538fa69f6f07
Gurkamal Singh <gurkamalpsc@gmail.com>
2019-05-03 00:28:39 +0000

    settting up App.java as a client

--- general-tree-data-structure/src/main/java/general_tree_data_structure/App.java
+++ general-tree-data-structure/src/main/java/general_tree_data_structure/App.java
@@ -1,9 +1,16 @@
 package general_tree_data_structure;
 
-public class App 
-{
-   public static void main(String[] args)
-   {
-      System.out.println("Running App.java for general");
-   }
+public class App {
+    public static void main(String[] args) throws Exception {
+        Tree<String> sceneTree = new Tree<String>();
+        TreeNode<String> tn;
+        
+        // create a scene in a room
+        //tn = sceneTree.addChild(null, "room");
+        
+        // add three objects to the scene tree
+        // sceneTree.addChild(tn, "Lily the canine");
+        // sceneTree.addChild(tn, "Miguel the human");
+        // sceneTree.addChild(tn, "table");
+    }
 } 
